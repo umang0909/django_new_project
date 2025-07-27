2683c3bac0461a5943d5f92117985905bd4172d6
umang0909 <32355401+umang0909@users.noreply.github.com>
2025-07-27 12:46:58 +0530

    Update views.py by adding new variable on line 7

--- myapp/views.py
+++ myapp/views.py
@@ -7,7 +7,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 # Create your views here.
-
+new_data = 1
 def book_list(request):
     logger.info("Book list view was accessed.")
     books = Book.objects.all()
